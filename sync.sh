b0b1cb28ead156e5aa0b1497a633ac0473810b36
Brooks Lybrand <brookslybrand@gmail.com>
2026-07-16 12:54:30 -0500

    Update docs generation path

--- sync.sh
+++ sync.sh
@@ -17,7 +17,7 @@ rm -rf docs/assets
 rm -rf docs/fragment
 rm -f docs/index.html
 
-pushd ../remix/docs
+pushd ../remix/api
 pnpm run docs --tag "${TAG_NAME}"
 pnpm run build
 pnpm run prerender --dir "${DOCS_DIR}"
